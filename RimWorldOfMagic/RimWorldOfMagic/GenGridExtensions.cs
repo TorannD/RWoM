659518914a6bef986b2d4b055742929c5d5526bc
Kolton Noreen <noreenko@users.noreply.github.com>
2022-06-13 00:56:00 -0500

    Change if statement to try catch as c can be null if the pawn is destroyed

--- RimWorldOfMagic/RimWorldOfMagic/GenGridExtensions.cs
+++ RimWorldOfMagic/RimWorldOfMagic/GenGridExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using System;
+using Verse;
 
 namespace TorannMagic
 {
@@ -6,7 +7,16 @@ namespace TorannMagic
     {
         public static bool InBoundsWithNullCheck(this IntVec3 c, Map map)
         {
-            return map != null && c.InBounds(map);
+            // Use try catch as there are only rare exceptions when c is deleted after call but before Inbounds is called
+            try
+            {
+                return c.InBounds(map);
+            }
+            catch (NullReferenceException e)
+            {
+                Log.Warning($"NullReference during InBounds call: {e.ToString()}");
+                return false;
+            }
         }
     }
 }
