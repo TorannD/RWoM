d2212ce7fab5672ec70dac8b32f082c1688cccdf
Kolton Noreen <noreenko@users.noreply.github.com>
2022-07-27 04:33:33 -0500

    move SimpleCache.cs into Utils Folder

--- RimWorldOfMagic/RimWorldOfMagic/Utils/SimpleCache.cs
+++ RimWorldOfMagic/RimWorldOfMagic/Utils/SimpleCache.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Verse;
 
-namespace TorannMagic
+namespace TorannMagic.Utils
 {
     public class SimpleCache<TKey, TValue>
     {
