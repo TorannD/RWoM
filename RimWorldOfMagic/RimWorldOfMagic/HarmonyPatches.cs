58953f8123cf878ceda90ce84ff5823b5b072f1b
Kolton Noreen <noreenko@users.noreply.github.com>
2022-07-29 23:24:04 -0500

    Simplify CheckRemoveMapNow_Patch

--- RimWorldOfMagic/RimWorldOfMagic/HarmonyPatches.cs
+++ RimWorldOfMagic/RimWorldOfMagic/HarmonyPatches.cs
@@ -6153,12 +6153,7 @@ namespace TorannMagic
         {
             public static bool Prefix()
             {
-                bool inFlight = ModOptions.Constants.GetPawnInFlight();
-                if (inFlight)
-                {
-                    return false;
-                }
-                return true;
+                return !ModOptions.Constants.GetPawnInFlight();
             }
         }
 
