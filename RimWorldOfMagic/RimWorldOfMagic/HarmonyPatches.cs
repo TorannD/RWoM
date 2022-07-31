e01a837e3b7791ec3df5a94a3da4245ce4f2aa5c
Kolton Noreen <noreenko@users.noreply.github.com>
2022-07-30 20:51:47 -0500

    Clean rendering code for the Marks and clean up a few TM_Calc functions

--- RimWorldOfMagic/RimWorldOfMagic/HarmonyPatches.cs
+++ RimWorldOfMagic/RimWorldOfMagic/HarmonyPatches.cs
@@ -6408,7 +6408,7 @@ namespace TorannMagic
                     {
                         if (colonist.health.hediffSet.HasHediff(TorannMagicDefOf.TM_UndeadHD))
                         {
-                            return new TraitIconMap.TraitIconValue(TM_MatPool.Icon_Undead, "TM_Icon_Undead");
+                            return new TraitIconMap.TraitIconValue(TM_RenderQueue.necroMarkMat, TM_MatPool.Icon_Undead, "TM_Icon_Undead");
                         }
 
                         // Early exit condition
@@ -6418,12 +6418,7 @@ namespace TorannMagic
                         {
                             TraitDef trait = colonist.story.traits.allTraits[i].def;
                             if (TraitIconMap.ContainsKey(trait))
-                            {
-                                return new TraitIconMap.TraitIconValue(
-                                    TraitIconMap.Get(trait).IconMaterial,
-                                    TraitIconMap.Get(trait).IconType
-                                );
-                            }
+                                return TraitIconMap.Get(trait);
                         }
 
                         return null;
@@ -6438,7 +6433,7 @@ namespace TorannMagic
                 float num = 20f * Find.ColonistBar.Scale * settingsRef.classIconSize;
                 Vector2 vector = new Vector2(rect.x + 1f, rect.yMin + 1f);
                 rect = new Rect(vector.x, vector.y, num, num);
-                GUI.DrawTexture(rect, traitIconValue.IconMaterial);
+                GUI.DrawTexture(rect, traitIconValue.IconTexture);
                 TooltipHandler.TipRegion(rect, traitIconValue.IconType.Translate());
                 vector.x += num;
             }
--- RimWorldOfMagic/RimWorldOfMagic/TraitIconMap.cs
+++ RimWorldOfMagic/RimWorldOfMagic/TraitIconMap.cs
@@ -12,12 +12,14 @@ namespace TorannMagic
         // Class to hold Values for below dictionary
         public class TraitIconValue
         {
-            public readonly Texture2D IconMaterial;
+            public readonly Material IconMaterial;
+            public readonly Texture2D IconTexture;
             public readonly string IconType;
 
-            public TraitIconValue(Texture2D iconMaterial, string iconType)
+            public TraitIconValue(Material iconMaterial, Texture2D iconTexture, string iconType)
             {
                 IconMaterial = iconMaterial;
+                IconTexture = iconTexture;
                 IconType = iconType;
             }
         }
@@ -25,36 +27,37 @@ namespace TorannMagic
         // via ModOptions.ModClassOptions.InitializeCustomClassActions
         private static readonly Dictionary<ushort, TraitIconValue> TraitIconMapping = new Dictionary<ushort, TraitIconValue>()
         {
-            { TorannMagicDefOf.InnerFire.index, new TraitIconValue(TM_MatPool.fireIcon, MageIcon) },
-            { TorannMagicDefOf.HeartOfFrost.index, new TraitIconValue(TM_MatPool.iceIcon, MageIcon) },
-            { TorannMagicDefOf.StormBorn.index, new TraitIconValue(TM_MatPool.lightningIcon, MageIcon) },
-            { TorannMagicDefOf.Arcanist.index, new TraitIconValue(TM_MatPool.arcanistIcon, MageIcon) },
-            { TorannMagicDefOf.Paladin.index, new TraitIconValue(TM_MatPool.paladinIcon, MageIcon) },
-            { TorannMagicDefOf.Summoner.index, new TraitIconValue(TM_MatPool.summonerIcon, MageIcon) },
-            { TorannMagicDefOf.Druid.index, new TraitIconValue(TM_MatPool.druidIcon, MageIcon) },
-            { TorannMagicDefOf.Necromancer.index, new TraitIconValue(TM_MatPool.necroIcon, MageIcon) },
-            { TorannMagicDefOf.Lich.index, new TraitIconValue(TM_MatPool.necroIcon, MageIcon) },
-            { TorannMagicDefOf.TM_Bard.index, new TraitIconValue(TM_MatPool.bardIcon, MageIcon) },
-            { TorannMagicDefOf.Succubus.index, new TraitIconValue(TM_MatPool.demonkinIcon, MageIcon) },
-            { TorannMagicDefOf.Warlock.index, new TraitIconValue(TM_MatPool.demonkinIcon, MageIcon) },
-            { TorannMagicDefOf.Geomancer.index, new TraitIconValue(TM_MatPool.earthIcon, MageIcon) },
-            { TorannMagicDefOf.Technomancer.index, new TraitIconValue(TM_MatPool.technoIcon, MageIcon) },
-            { TorannMagicDefOf.BloodMage.index, new TraitIconValue(TM_MatPool.bloodmageIcon, MageIcon) },
-            { TorannMagicDefOf.Enchanter.index, new TraitIconValue(TM_MatPool.enchanterIcon, MageIcon) },
-            { TorannMagicDefOf.Chronomancer.index, new TraitIconValue(TM_MatPool.chronoIcon, MageIcon) },
-            { TorannMagicDefOf.Gladiator.index, new TraitIconValue(TM_MatPool.gladiatorIcon, FighterIcon) },
-            { TorannMagicDefOf.TM_Sniper.index, new TraitIconValue(TM_MatPool.sniperIcon, FighterIcon) },
-            { TorannMagicDefOf.Bladedancer.index, new TraitIconValue(TM_MatPool.bladedancerIcon, FighterIcon) },
-            { TorannMagicDefOf.Ranger.index, new TraitIconValue(TM_MatPool.rangerIcon, FighterIcon) },
-            { TorannMagicDefOf.Faceless.index, new TraitIconValue(TM_MatPool.facelessIcon, FighterIcon) },
-            { TorannMagicDefOf.TM_Psionic.index, new TraitIconValue(TM_MatPool.psiIcon, FighterIcon) },
-            { TorannMagicDefOf.DeathKnight.index, new TraitIconValue(TM_MatPool.deathknightIcon, FighterIcon) },
-            { TorannMagicDefOf.TM_Monk.index, new TraitIconValue(TM_MatPool.monkIcon, FighterIcon) },
-            { TorannMagicDefOf.TM_Wanderer.index, new TraitIconValue(TM_MatPool.wandererIcon, MageIcon) },
-            { TorannMagicDefOf.TM_Wayfarer.index, new TraitIconValue(TM_MatPool.wayfarerIcon, FighterIcon) },
-            { TorannMagicDefOf.ChaosMage.index, new TraitIconValue(TM_MatPool.chaosIcon, MageIcon) },
-            { TorannMagicDefOf.TM_Commander.index, new TraitIconValue(TM_MatPool.commanderIcon, FighterIcon) },
-            { TorannMagicDefOf.TM_SuperSoldier.index, new TraitIconValue(TM_MatPool.SSIcon, FighterIcon) }
+            { TorannMagicDefOf.InnerFire.index, new TraitIconValue(TM_RenderQueue.fireMarkMat, TM_MatPool.fireIcon, MageIcon) },
+            { TorannMagicDefOf.HeartOfFrost.index, new TraitIconValue(TM_RenderQueue.iceMarkMat, TM_MatPool.iceIcon, MageIcon) },
+            { TorannMagicDefOf.StormBorn.index, new TraitIconValue(TM_RenderQueue.lightningMarkMat, TM_MatPool.lightningIcon, MageIcon) },
+            { TorannMagicDefOf.Arcanist.index, new TraitIconValue(TM_RenderQueue.arcanistMarkMat, TM_MatPool.arcanistIcon, MageIcon) },
+            { TorannMagicDefOf.Paladin.index, new TraitIconValue(TM_RenderQueue.paladinMarkMat, TM_MatPool.paladinIcon, MageIcon) },
+            { TorannMagicDefOf.Priest.index, new TraitIconValue(TM_RenderQueue.priestMarkMat, TM_MatPool.priestIcon, MageIcon) },
+            { TorannMagicDefOf.Summoner.index, new TraitIconValue(TM_RenderQueue.summonerMarkMat, TM_MatPool.summonerIcon, MageIcon) },
+            { TorannMagicDefOf.Druid.index, new TraitIconValue(TM_RenderQueue.druidMarkMat, TM_MatPool.druidIcon, MageIcon) },
+            { TorannMagicDefOf.Necromancer.index, new TraitIconValue(TM_RenderQueue.necroMarkMat, TM_MatPool.necroIcon, MageIcon) },
+            { TorannMagicDefOf.Lich.index, new TraitIconValue(TM_RenderQueue.necroMarkMat, TM_MatPool.necroIcon, MageIcon) },
+            { TorannMagicDefOf.TM_Bard.index, new TraitIconValue(TM_RenderQueue.bardMarkMat, TM_MatPool.bardIcon, MageIcon) },
+            { TorannMagicDefOf.Succubus.index, new TraitIconValue(TM_RenderQueue.demonkinMarkMat, TM_MatPool.demonkinIcon, MageIcon) },
+            { TorannMagicDefOf.Warlock.index, new TraitIconValue(TM_RenderQueue.warlockMarkMat, TM_MatPool.demonkinIcon, MageIcon) },
+            { TorannMagicDefOf.Geomancer.index, new TraitIconValue(TM_RenderQueue.earthMarkMat, TM_MatPool.earthIcon, MageIcon) },
+            { TorannMagicDefOf.Technomancer.index, new TraitIconValue(TM_RenderQueue.technoMarkMat, TM_MatPool.technoIcon, MageIcon) },
+            { TorannMagicDefOf.BloodMage.index, new TraitIconValue(TM_RenderQueue.bloodmageMarkMat, TM_MatPool.bloodmageIcon, MageIcon) },
+            { TorannMagicDefOf.Enchanter.index, new TraitIconValue(TM_RenderQueue.enchanterMarkMat, TM_MatPool.enchanterIcon, MageIcon) },
+            { TorannMagicDefOf.Chronomancer.index, new TraitIconValue(TM_RenderQueue.chronomancerMarkMat, TM_MatPool.chronoIcon, MageIcon) },
+            { TorannMagicDefOf.Gladiator.index, new TraitIconValue(TM_RenderQueue.gladiatorMarkMat, TM_MatPool.gladiatorIcon, FighterIcon) },
+            { TorannMagicDefOf.TM_Sniper.index, new TraitIconValue(TM_RenderQueue.sniperMarkMat, TM_MatPool.sniperIcon, FighterIcon) },
+            { TorannMagicDefOf.Bladedancer.index, new TraitIconValue(TM_RenderQueue.bladedancerMarkMat, TM_MatPool.bladedancerIcon, FighterIcon) },
+            { TorannMagicDefOf.Ranger.index, new TraitIconValue(TM_RenderQueue.rangerMarkMat, TM_MatPool.rangerIcon, FighterIcon) },
+            { TorannMagicDefOf.Faceless.index, new TraitIconValue(TM_RenderQueue.facelessMarkMat, TM_MatPool.facelessIcon, FighterIcon) },
+            { TorannMagicDefOf.TM_Psionic.index, new TraitIconValue(TM_RenderQueue.psionicMarkMat, TM_MatPool.psiIcon, FighterIcon) },
+            { TorannMagicDefOf.DeathKnight.index, new TraitIconValue(TM_RenderQueue.deathknightMarkMat, TM_MatPool.deathknightIcon, FighterIcon) },
+            { TorannMagicDefOf.TM_Monk.index, new TraitIconValue(TM_RenderQueue.monkMarkMat, TM_MatPool.monkIcon, FighterIcon) },
+            { TorannMagicDefOf.TM_Wanderer.index, new TraitIconValue(TM_RenderQueue.wandererMarkMat, TM_MatPool.wandererIcon, MageIcon) },
+            { TorannMagicDefOf.TM_Wayfarer.index, new TraitIconValue(TM_RenderQueue.wayfarerMarkMat, TM_MatPool.wayfarerIcon, FighterIcon) },
+            { TorannMagicDefOf.ChaosMage.index, new TraitIconValue(TM_RenderQueue.chaosMarkMat, TM_MatPool.chaosIcon, MageIcon) },
+            { TorannMagicDefOf.TM_Commander.index, new TraitIconValue(TM_RenderQueue.commanderMarkMat, TM_MatPool.commanderIcon, FighterIcon) },
+            { TorannMagicDefOf.TM_SuperSoldier.index, new TraitIconValue(TM_RenderQueue.supersoldierMarkMat, TM_MatPool.SSIcon, FighterIcon) }
         };
 
         public static TraitIconValue Get(TraitDef traitDef)
