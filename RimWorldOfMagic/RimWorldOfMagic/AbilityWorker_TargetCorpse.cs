b80a5b54381fb35665142d8b19dd574271baf8c7
Kolton Noreen <noreenko@users.noreply.github.com>
2022-10-05 00:55:30 -0500

    Clean AbilityWorker_TargetCorpse.cs

--- RimWorldOfMagic/RimWorldOfMagic/AbilityWorker_TargetCorpse.cs
+++ RimWorldOfMagic/RimWorldOfMagic/AbilityWorker_TargetCorpse.cs
@@ -9,82 +9,35 @@ namespace TorannMagic
     {
         public override LocalTargetInfo TargetAbilityFor(AbilityAIDef abilityDef, Pawn pawn)
         {
-            Corpse corpse = this.PickClosestCorpse(abilityDef, pawn);
-            bool flag = corpse == null;
-            LocalTargetInfo result;
-            if (flag)
-            {
-                result = base.TargetAbilityFor(abilityDef, pawn);
-            }
-            else
-            {
-                result = corpse;
-            }
-            return result;
+            Corpse corpse = PickClosestCorpse(abilityDef, pawn);
+            return corpse ?? base.TargetAbilityFor(abilityDef, pawn);
         }
 
         public override bool CanPawnUseThisAbility(AbilityAIDef abilityDef, Pawn pawn, LocalTargetInfo target)
         {
-            Corpse corpse = this.PickClosestCorpse(abilityDef, pawn);
-            bool flag = corpse == null;
-            return !flag && base.CanPawnUseThisAbility(abilityDef, pawn, target);
+            Corpse corpse = PickClosestCorpse(abilityDef, pawn);
+            return corpse != null && base.CanPawnUseThisAbility(abilityDef, pawn, target);
         }
 
         public virtual Corpse PickClosestCorpse(AbilityAIDef abilityDef, Pawn pawn)
         {
-            Corpse corpse = null;
-            Thing corpseThing = null;
-
-            IntVec3 curCell;
-            IEnumerable<IntVec3> targets = GenRadial.RadialCellsAround(pawn.Position, 6f, true);
-            for (int i = 0; i < targets.Count(); i++)
+            foreach (IntVec3 curCell in GenRadial.RadialCellsAround(pawn.TargetCurrentlyAimingAt.Cell, 6f, true))
             {
-                curCell = targets.ToArray<IntVec3>()[i];
-                if (curCell.InBoundsWithNullCheck(pawn.Map) && curCell.IsValid)
-                {
-                    List<Thing> thingList;
-                    thingList = curCell.GetThingList(pawn.Map);
-                    int z = 0;
-                    while (z < thingList.Count)
-                    {
-                        corpseThing = thingList[z];
-                        if (corpseThing != null)
-                        {
-                            bool validator = corpseThing is Corpse;
-                            if (validator)
-                            {
-                                corpse = corpseThing as Corpse;
-                            }                            
-                        }
-                    }
-                }
+                if (!curCell.InBoundsWithNullCheck(pawn.Map) || !curCell.IsValid) continue;
+
+                Corpse corpse = curCell.GetThingList(pawn.Map).OfType<Corpse>().FirstOrDefault();
+                if (corpse != null) return corpse;
             }
 
-            IEnumerable<IntVec3> targetsD = GenRadial.RadialCellsAround(pawn.TargetCurrentlyAimingAt.Cell, 6f, true);
-            for (int i = 0; i < targets.Count(); i++)
+            foreach (IntVec3 curCell in GenRadial.RadialCellsAround(pawn.Position, 6f, true))
             {
-                curCell = targets.ToArray<IntVec3>()[i];
-                if (curCell.InBoundsWithNullCheck(pawn.Map) && curCell.IsValid)
-                {
-                    List<Thing> thingList;
-                    thingList = curCell.GetThingList(pawn.Map);
-                    int z = 0;
-                    while (z < thingList.Count)
-                    {
-                        corpseThing = thingList[z];
-                        if (corpseThing != null)
-                        {
-                            bool validator = corpseThing is Corpse;
-                            if (validator)
-                            {
-                                corpse = corpseThing as Corpse;
-                            }
-                        }
-                    }
-                }
+                if (!curCell.InBoundsWithNullCheck(pawn.Map) || !curCell.IsValid) continue;
+
+                Corpse corpse = curCell.GetThingList(pawn.Map).OfType<Corpse>().FirstOrDefault();
+                if (corpse != null) return corpse;
             }
 
-            return corpse;
+            return null;
         }
     }
 }
