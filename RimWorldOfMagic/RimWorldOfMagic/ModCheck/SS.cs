78ccb775ab944ad227e4ee8b225892940f5a26f4
Mateus Pedroso Zanini <programmer.mateus@gmail.com>
2025-07-12 14:26:02 -0300

    disable SimpleSidearms patch

--- RimWorldOfMagic/RimWorldOfMagic/ModCheck/SS.cs
+++ RimWorldOfMagic/RimWorldOfMagic/ModCheck/SS.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using Verse;
 using RimWorld;
-using SimpleSidearms;
-using SimpleSidearms.rimworld;
+// using SimpleSidearms;
+// using SimpleSidearms.rimworld;
 
 namespace TorannMagic.ModCheck
 {
     public static class SS
     {
-        public static void ClearWeaponMemory(Pawn p)
-        {
-            CompSidearmMemory csm = p.TryGetComp<CompSidearmMemory>();
-            if (csm != null && csm.rememberedWeapons != null && csm.RememberedWeapons.Count > 0 && p.equipment != null)
-            {
-                while(csm.RememberedWeapons.Count > 0)
-                {
-                    csm.ForgetSidearmMemory(csm.RememberedWeapons[0]);
-                }
-                csm.GenerateRememberedWeaponsFromEquipped();
-            }
-        }
+        // public static void ClearWeaponMemory(Pawn p)
+        // {
+        //     CompSidearmMemory csm = p.TryGetComp<CompSidearmMemory>();
+        //     if (csm != null && csm.rememberedWeapons != null && csm.RememberedWeapons.Count > 0 && p.equipment != null)
+        //     {
+        //         while(csm.RememberedWeapons.Count > 0)
+        //         {
+        //             csm.ForgetSidearmMemory(csm.RememberedWeapons[0]);
+        //         }
+        //         csm.GenerateRememberedWeaponsFromEquipped();
+        //     }
+        // }
     }
 }
